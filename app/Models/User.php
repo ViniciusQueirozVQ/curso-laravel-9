20747d831c91166e84cdb530cde0b817d8a8a0e5
ViniciusQueirozVQ <viniciusqueiroz.voq@gmail.com>
2022-06-01 21:12:27 -0300

    Cadastrar novo comentário

--- app/Models/User.php
+++ app/Models/User.php
@@ -55,4 +55,8 @@ class User extends Authenticatable
 
          return $users;
     }
+
+    public function comments(){
+        return $this->hasMany(Comment::class);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,23 @@
+
 <?php
 
 use App\Http\Controllers\{
     UserController
 };
+use App\Http\Controllers\Admin\CommentController;
 use Illuminate\Support\Facades\Route;
 
+Route::post('/users/{id}/comments', [CommentController::class, 'store'])->name('comments.store');
+Route::get('/users/{id}/comments/create', [CommentController::class, 'create'])->name('comments.create');
+Route::get('/users/{id}/comments', [CommentController::class, 'index'])->name('comments.index');
+
 Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
-Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
 Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
+Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
 Route::get('/users', [UserController::class, 'index'])->name('users.index');
 Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
 Route::post('/users', [UserController::class, 'store'])->name('users.store');
 Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
-
 
 Route::get('/', function () {
     return view('welcome');
